20dacaae82f9c01f8c5a9186686bb9eeb140e573
Logan Alvarez <logan.alvarez616@gmail.com>
2021-12-10 10:28:24 -0500

    changed a note

--- API/AJAX-Lab/script.js
+++ API/AJAX-Lab/script.js
@@ -6,7 +6,7 @@ fetch("https://www.reddit.com/r/aww/.json") //Fetch the data from link
 
 .then(data => { 
     // console.log(data)
-    for(let i = 0; i < data.data.children.length; i++){ //Then take the JSON DATA and iterate through the children length.
+    for(let i = 0; i < data.data.children.length; i++){ //Then take the JSON DATA and iterate through the "children data" length.
          const newDiv = document.createElement("div")
          let Names = data.data.children[i].data.name
          let title = data.data.children[i].data.title //Grab the value from each child and assign it a variable to be used later. IMPORTANT to remember you can do this anytime!!
