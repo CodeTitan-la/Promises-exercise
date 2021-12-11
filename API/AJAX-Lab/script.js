ac545720c1997b66f47193417650f35afbfd2564
Logan Alvarez <logan.alvarez616@gmail.com>
2021-12-10 20:14:39 -0500

    Re-did the lab - finished

--- API/AJAX-Lab/script.js
+++ API/AJAX-Lab/script.js
@@ -6,15 +6,39 @@ fetch("https://www.reddit.com/r/aww/.json") //Fetch the data from link
 
 .then(data => { 
     // console.log(data)
-    for(let i = 0; i < data.data.children.length; i++){ //Then take the JSON DATA and iterate through the "children data" length.
-         const newDiv = document.createElement("div")
-         let Names = data.data.children[i].data.name
-         let title = data.data.children[i].data.title //Grab the value from each child and assign it a variable to be used later. IMPORTANT to remember you can do this anytime!!
-         let link = data.data.children[i].data.url
-         let image = data.data.children[i].data.permalink
-        newDiv.innerHTML = `Name: ${Names} <br> Title: ${title} <br> Link: ${link} <br> Image: ${image} ` //change the inner text each newDIV inside of the HTML doc 
-        console.log(newDiv);
-        body.appendChild(newDiv); //Take the newDiv and insert it into the HTML file using the DOM.
+    for(let i = 0; i < data.data.children.length; i++){ //Then take the JSON DATA and iterate through the "children data" length. 
+        
+        const section = document.getElementById("section")//Create new section container for every object
+        //Title of post
+        const title = data.data.children[i].data.title; //Grab the value from each child and assign it a variable to be used later. IMPORTANT to remember you can do this anytime!!  Creat H2 for Title and create somewhere to put it
+        const postHeading = document.createElement("h2");
+        postHeading.innerText = title;//add title to h2
+        section.append(postHeading); //append h2
+        //Name of user
+        const Names = data.data.children[i].data.name;//Create h3 for Name of poster
+        const nameOfUser = document.createElement("h3");
+        nameOfUser.innerText = Names;// Insert Names into the H3 element
+        section.append(nameOfUser);//append completed H3 element into the section
+        //Image
+        let image = data.data.children[i].data.thumbnail; //Create img element
+        const postsImage = document.createElement("img");
+        postsImage.setAttribute("src", image);
+        section.append(postsImage);
+       //Link
+        let link = data.data.children[i].data.permalink;//create anchor element and grab the data
+        let url = "http://reddit.com" + link
+        const newLinkPara = document.createElement("p")
+        const newLink = document.createElement("a");
+        newLink.setAttribute("href", url)
+        newLink.target ="_blank";
+        newLink.innerText = "Click to follow link to Reddit!"
+        newLinkPara.append(newLink);
+        section.append(newLink);
+        
+
+
+
+        
     }  
 })
 
